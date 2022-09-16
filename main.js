3208f85807ef248c6f267d3d7783ffa3d91172a3
Márian Wiermann <mwcasas@hotmail.com>
2022-09-16 11:32:58 -0300

    add funcoes js

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
+// app -----------
 const cards = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
 
+// cards -----------
 cards.forEach(card => {
-  card.addEventListener('dragstart', dragstar)
+  card.addEventListener('dragstart', dragstart)
   card.addEventListener('drag', drag)
   card.addEventListener('dragend', dragend)
 })
+
+function dragstart() {
+  // console.log('> card: Star dragging')
+  dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
+  this.classList.add('is-dragging')
+}
+
+function drag() {
+  // console.log('> card: Is dragging')
+}
+
+function dragend() {
+  // console.log('> card: Stop dragging')
+  dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
+  this.classList.remove('is-dragging')
+}
+
+// dropzone -----------
+dropzones.forEach(dropzone => {
+  dropzone.addEventListener('dragenter', dragenter)
+  dropzone.addEventListener('dragover', dragover)
+  dropzone.addEventListener('dragleave', dragleave)
+  dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+  // console.log('> dropzone: Enter in zone')
+}
+
+function dragover() {
+  // console.log('> dropzone: Over')
+  this.classList.add('over')
+}
+
+function dragleave() {
+  // console.log('> dropzone: Leave')
+  this.classList.remove('over')
+}
+
+function drop() {}
