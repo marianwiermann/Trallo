8ed085a424fad252fe0abd4d6db7b256b92a78e7
Marian W <mwcasas@hotmail.com>
2022-12-12 08:15:29 -0300

    add caixa para novos cards

--- main.js
+++ main.js
@@ -45,3 +45,27 @@ function dragleave() {
 function drop() {
   this.classList.remove('over')
 }
+
+// add cards -----------
+const botaoAdicionar = document.querySelector('.botao-add')
+
+botaoAdicionar.addEventListener('click', function (event) {
+  event.preventDefault()
+
+  // pegando o valor o novo conteudo
+  const form = document.querySelector('#form-adiciona')
+  const conteudo = form.texto.value
+
+  // novo card
+  const novoCard = document.createElement('div')
+  const novoConteudo = document.createElement('div')
+  novoCard.classList.add('card') //add class
+
+  novoConteudo.textContent = conteudo
+
+  novoCard.appendChild(novoConteudo)
+
+  const board = document.querySelector('.dropzone')
+
+  board.appendChild(novoCard)
+})
